80b74ffc734ecb185f20459998a0c5601abe7713
tetantes <160345462+tetantes@users.noreply.github.com>
2026-05-19 14:44:24 +0100

    Add APP_CONSTANTS for application configuration

---
+++ src/libs/constant.ts
@@ -0,0 +1,9 @@
+export const APP_CONSTANTS = {
+  DAILY_BONUS_REWARD_POINTS: 250,
+  DAILY_BONUS_REWARD_NGN: 10,
+  REFERRAL_REWARD_PERCENTAGE: 10, // 10% of referral task earnings
+  MIN_WITHDRAWAL_NGN: 2000,
+  MAX_WITHDRAWAL_NGN: 50000,
+  TELEGRAM_CHANNEL_URL: 'https://t.me/NairaEarnProOfficial',
+  SUPPORT_BOT_URL: 'https://t.me/NairaEarnSupportBot',
+};
